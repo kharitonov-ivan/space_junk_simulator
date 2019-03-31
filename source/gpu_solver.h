c81b3705cc0ded770ce3e6cd692cf8d499f148b1
EGor <bokoveg@yandex.ru>
2019-03-31 19:33:46 +0300

    gpu_solver (still some problems) + ipynb with simple solver run

---
+++ source/gpu_solver.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "model.h"
+
+namespace GPUSolver {
+    class Solver : public World::Solver {
+    public:
+        Solver() {};
+
+
+        void Solve(double dt,
+            double time,
+            std::vector<World::Object>& objects,
+            std::vector<World::Force>& forces,
+            size_t stepsNumber,
+            std::vector<std::vector<World::Object> >& positions);
+    };
+}
