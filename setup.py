aaafbed2cb280a1660b5f2015dde2681871b6faf
ivan_kharitonov <neer201@gmail.com>
2019-04-03 16:32:11 +0300

    added setup py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='space_junk_simulator',
+    version='',
+    packages=[''],
+    url='',
+    license='',
+    author='ivan_kharitonov',
+    author_email='',
+    description=''
+)
